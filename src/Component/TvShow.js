3e71bddbd321e456be626079699adc7c19f77bb7
aditi <aditi291soni@gmail.com>
2022-09-07 02:08:53 +0530

    Tvshow

--- src/Component/TvShow.js
+++ src/Component/TvShow.js
@@ -7,7 +7,6 @@ import '../Component/Style.css'
 
 
 const TvShow = ({tvshows, settvshows}) => {
-  // const [tvshows, settvshows] = useState([]);
 
   const nav=useNavigate();
   const idsH=(i)=>{
@@ -16,9 +15,9 @@ nav(`/TvShow/${i}`)
   const a=()=>{
     const b = axios.get("https://api.themoviedb.org/3/tv/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US ")
     .then(res=>settvshows(res.data.results))
-    // .then(settvshows(b))
    }
    a()
+   
   return (
     <div className='movie'>
       
@@ -32,7 +31,7 @@ nav(`/TvShow/${i}`)
         <div key={i} onClick={()=>idsH(e.id)} className="card">
         
         <img src={`https://image.tmdb.org/t/p/original/${e&& e.backdrop_path}`} alt="" />
-        <h2>{e.title}</h2>
+        <h2>{e.name}</h2>
     </div>
     ))
 }
--- src/Component/Tvsingle.js
+++ src/Component/Tvsingle.js
@@ -3,7 +3,6 @@ import { useParams } from 'react-router-dom';
 import '../Component/Style.css'
 
 const Tvsingle = ({tvshows, settvshows}) => {
-  // console.log(popular);
   let { id } = useParams();
   return (
     <div className="single">
@@ -18,10 +17,10 @@ if(elem.id==id){
       </div>
       <div className="task">
  
-      <h2>Title <br />{elem.title}</h2> <br />
+      <h2>Title <br />{elem.name}</h2> <br />
         
-        <h2>Original title <br />{elem.original_title}</h2> <br />
-        <h2>Release Date <br /> {elem.release_date}</h2> <br />
+        <h2>Original title <br />{elem.original_name}</h2> <br />
+        <h2>Release Date <br /> {elem.first_air_date}</h2> <br />
         <h2>Language <br /> {elem.original_language}</h2> <br />
         <h3>Overview <br />{elem.overview}</h3>
       </div>
